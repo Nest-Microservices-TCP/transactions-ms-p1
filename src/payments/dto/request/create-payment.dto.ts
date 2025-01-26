2daa6a20ce9097e7298f03639431333d27465614
JosueEC <josueev.cruz@gmail.com>
2025-01-25 21:17:13 -0600

    [FEAT](payments/dto/request): Add the create-payment-dto with properties validations

---
+++ src/payments/dto/request/create-payment.dto.ts
@@ -0,0 +1,11 @@
+import { IsNumber, IsPositive, IsUUID, Min } from 'class-validator';
+
+export class CreatePaymentDto {
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  total: number;
+
+  @IsUUID()
+  ticket_id: string;
+}
