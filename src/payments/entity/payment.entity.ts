b146ff1a6e18a3c34d6e893dc6b59f8b17050dae
JosueEC <josueev.cruz@gmail.com>
2025-01-25 21:13:44 -0600

    [FEAT](transactions/entity): Add the payment entity class and columns configuration

---
+++ src/payments/entity/payment.entity.ts
@@ -0,0 +1,23 @@
+import { BaseEntity } from 'src/common/entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'payments' })
+export class Payment extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'payment_id',
+    primaryKeyConstraintName: 'PK_payments',
+  })
+  paymentId: string;
+
+  @Column({
+    name: 'total',
+    type: 'numeric',
+    precision: 9,
+    scale: 2,
+    nullable: false,
+  })
+  total: number;
+
+  //TODO: Definir la relación 1:1 con tickets
+  ticket_id: string;
+}
