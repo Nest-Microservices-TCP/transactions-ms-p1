081e761da87b8290f127083f0ec9bcaf57ba2a2f
JosueEC <josueev.cruz@gmail.com>
2025-01-27 19:12:23 -0600

    [FEAT](payments.repository): Implements the rest of repository methods

--- src/payments/repository/payments.repository.ts
+++ src/payments/repository/payments.repository.ts
@@ -2,13 +2,14 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindOptionsWhere, QueryRunner, Repository } from 'typeorm';
 
+import { EntityNotFoundException } from 'src/common/exceptions/custom';
+
 import { DeleteResultResponse } from 'src/common/dto/response';
 import { CreatePaymentDto } from '../dto/request';
 
 import { Payment } from '../entity/payment.entity';
 
 import { IPaymentsRepository } from './interfaces/payments.repository.interface';
-import { EntityNotFoundException } from 'src/common/exceptions/custom';
 
 export class PaymentsRepository implements IPaymentsRepository {
   private paymentsRepository: Repository<Payment>;
@@ -57,6 +58,39 @@ export class PaymentsRepository implements IPaymentsRepository {
     throw new Error('Method not implemented.');
   }
   remove(id: string): Promise<DeleteResultResponse> {
+    throw new Error('Method not implemented.');
+  }
+  findByIds(ids: string[]): Promise<Payment[]> {
+    throw new Error('Method not implemented.');
+  }
+  findByCriteria(criteria: FindOptionsWhere<Payment>): Promise<Payment> {
+    throw new Error('Method not implemented.');
+  }
+  findWithRelations(relations: string[]): Promise<Payment[]> {
+    throw new Error('Method not implemented.');
+  }
+  count(criteria: FindOptionsWhere<Payment>): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+  paginate(page: number, limit: number): Promise<[Payment[], number]> {
+    throw new Error('Method not implemented.');
+  }
+  softDelete(id: string): Promise<Payment> {
+    throw new Error('Method not implemented.');
+  }
+  restore(id: string): Promise<Payment> {
+    throw new Error('Method not implemented.');
+  }
+  exists(criteria: FindOptionsWhere<Payment>): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  bulkSave(entities: Payment[]): Promise<Payment[]> {
+    throw new Error('Method not implemented.');
+  }
+  bulkUpdate(entities: Payment[]): Promise<Payment[]> {
+    throw new Error('Method not implemented.');
+  }
+  customQuery(query: string, params: any[]): Promise<any> {
     throw new Error('Method not implemented.');
   }
 }
