7cdf1a15d3af65482080ee978e23c3fad23adaff
JosueEC <josueev.cruz@gmail.com>
2025-01-27 19:11:46 -0600

    [REFACTOR](payments.repository.interface): Extends from the repository-interface instead of base-repository-interface

--- src/payments/repository/interfaces/payments.repository.interface.ts
+++ src/payments/repository/interfaces/payments.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IBaseRepository } from 'src/common/repository';
+import { IRepository } from 'src/common/repository';
 import { CreatePaymentDto } from 'src/payments/dto/request/create-payment.dto';
 import { Payment } from 'src/payments/entity/payment.entity';
 
 export interface IPaymentsRepository
-  extends IBaseRepository<Payment, CreatePaymentDto> {}
+  extends IRepository<Payment, CreatePaymentDto> {}
