69b9243e829db0971f32886eb47cc33191c49ea2
JosueEC <josueev.cruz@gmail.com>
2025-01-30 19:34:24 -0600

    [FEAT](payments.service): Add the 'plainToInstanceDto' service method

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+
+import { PaymentResponseDto } from './dto/response/payment.response.dto';
 
 import { PaymentsRepository } from './repository/payments.repository';
 
 @Injectable()
 export class PaymentsService {
   constructor(private readonly paymentsRepository: PaymentsRepository) {}
+
+  plainToInstanceDto(data: unknown) {
+    return plainToInstance(PaymentResponseDto, data, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
