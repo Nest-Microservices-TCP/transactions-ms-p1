1a0ee3fa0719674fc02dd92aa1c768888ec5cbb4
JosueEC <josueev.cruz@gmail.com>
2025-01-31 20:18:30 -0600

    [FEAT](payments.controller): Expose the find-all controller endpoint

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,7 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+
+import { PaymentResponseDto } from './dto/response';
+
 import { PaymentsService } from './payments.service';
 
 @Controller()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @MessagePattern({ cmd: 'find.all.payments' })
+  async findAll(): Promise<PaymentResponseDto[]> {
+    return this.paymentsService.findAll();
+  }
 }
