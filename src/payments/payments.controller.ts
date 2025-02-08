6e4c1cd7de6d680311bdd2a9a7b9e120555cbedf
JosueEC <josueev.cruz@gmail.com>
2025-02-07 22:11:34 -0600

    [REFACTOR](payments.controller): Replace the command message by topic message-patter for use it with kafka

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,8 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { MessagePattern } from '@nestjs/microservices';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreatePaymentDto, UpdatePaymentDto } from './dto/request';
 import { PaymentResponseDto } from './dto/response';
 
 import { PaymentsService } from './payments.service';
@@ -11,43 +9,8 @@ import { PaymentsService } from './payments.service';
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @MessagePattern({ cmd: 'find.all.payments' })
+  @MessagePattern('transactions.find.all.payments')
   async findAll(): Promise<PaymentResponseDto[]> {
     return this.paymentsService.findAll();
-  }
-
-  @MessagePattern({ cmd: 'find.one.payment' })
-  async findOne(
-    @Payload('paymentId') paymentId: string,
-  ): Promise<PaymentResponseDto> {
-    return this.paymentsService.findOne(paymentId);
-  }
-
-  @MessagePattern({ cmd: 'find.payments.by.ids' })
-  async findByIds(
-    @Payload() paymentsIds: string[],
-  ): Promise<PaymentResponseDto[]> {
-    return this.paymentsService.findByIds(paymentsIds);
-  }
-
-  @MessagePattern({ cmd: 'save.payment' })
-  async save(
-    @Payload() request: CreatePaymentDto,
-  ): Promise<PaymentResponseDto> {
-    return this.paymentsService.save(request);
-  }
-
-  @MessagePattern({ cmd: 'update.payment' })
-  async update(
-    @Payload() request: UpdatePaymentDto,
-  ): Promise<PaymentResponseDto> {
-    return this.paymentsService.update(request);
-  }
-
-  @MessagePattern({ cmd: 'remove.payment' })
-  async remove(
-    @Payload('paymentId') paymentId: string,
-  ): Promise<DeleteResultResponse> {
-    return this.paymentsService.remove(paymentId);
   }
 }
