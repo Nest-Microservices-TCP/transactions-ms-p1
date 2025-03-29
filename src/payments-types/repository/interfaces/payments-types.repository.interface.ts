961b6afb5259943992b9f330b6bea0ec42a08f87
JosueEC <josueev.cruz@gmail.com>
2025-03-28 21:59:56 -0600

    [FEAT](payments-types.repository.interface): Extends from the base-repository interface

--- src/payments-types/repository/interfaces/payments-types.repository.interface.ts
+++ src/payments-types/repository/interfaces/payments-types.repository.interface.ts
@@ -1,1 +1,6 @@
-export interface IPaymentsTypesRepository {}
+import { IBaseRepository } from 'src/common/repository';
+import { CreatePaymentTypeRequest } from 'src/grpc/proto/transactions/payments_types.pb';
+import { PaymentType } from 'src/payments-types/entity/payment-type.entity';
+
+export interface IPaymentsTypesRepository
+  extends IBaseRepository<PaymentType, CreatePaymentTypeRequest> {}
