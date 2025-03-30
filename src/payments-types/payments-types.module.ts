76732d8edaf9ee0dd7890c9c30335d518844e318
JosueEC <josueev.cruz@gmail.com>
2025-03-29 22:34:18 -0600

    [FEAT](payments-types.module): Inject the controllers module

--- src/payments-types/payments-types.module.ts
+++ src/payments-types/payments-types.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { PaymentsTypesService } from './payments-types.service';
+import { PaymentsTypesController } from './payments-types.controller';
 import { PaymentsTypesRepository } from './repository/payments-types.repository';
 
 @Module({
   providers: [PaymentsTypesRepository, PaymentsTypesService],
+  controllers: [PaymentsTypesController],
 })
 export class PaymentsTypesModule {}
