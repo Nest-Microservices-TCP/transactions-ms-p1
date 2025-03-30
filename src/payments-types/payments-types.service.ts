a66bb461b7beb8a9f1d8d2fb70a1a96f53688cbb
JosueEC <josueev.cruz@gmail.com>
2025-03-29 21:43:59 -0600

    [FEAT](payments-types.service): Define the save repository method

--- src/payments-types/payments-types.service.ts
+++ src/payments-types/payments-types.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { HandleRpcExceptions } from 'src/common/decorators';
+
+import { CreatePaymentTypeRequest } from 'src/grpc/proto/transactions/payments_types.pb';
+
 import { PaymentsTypesRepository } from './repository/payments-types.repository';
 
 @Injectable()
@@ -6,4 +10,9 @@ export class PaymentsTypesService {
   constructor(
     private readonly paymentsTypesRepository: PaymentsTypesRepository,
   ) {}
+
+  @HandleRpcExceptions()
+  async save(request: CreatePaymentTypeRequest): Promise<void> {
+    await this.paymentsTypesRepository.save(request);
+  }
 }
