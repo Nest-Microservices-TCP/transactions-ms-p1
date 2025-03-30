efbcdf7e1f6060b8e9593bc94f45439e0f5d43e1
JosueEC <josueev.cruz@gmail.com>
2025-03-29 21:47:07 -0600

    [FEAT](payments-types.service): Define the find-one repository method

--- src/payments-types/payments-types.service.ts
+++ src/payments-types/payments-types.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { HandleRpcExceptions } from 'src/common/decorators';
 
+import { PaymentsTypesRepository } from './repository/payments-types.repository';
 import { CreatePaymentTypeRequest } from 'src/grpc/proto/transactions/payments_types.pb';
 
-import { PaymentsTypesRepository } from './repository/payments-types.repository';
+import { PaymentType } from './entity/payment-type.entity';
 
 @Injectable()
 export class PaymentsTypesService {
@@ -14,5 +15,10 @@ export class PaymentsTypesService {
   @HandleRpcExceptions()
   async save(request: CreatePaymentTypeRequest): Promise<void> {
     await this.paymentsTypesRepository.save(request);
+  }
+
+  @HandleRpcExceptions()
+  async findOne(payment_type_id: string): Promise<PaymentType> {
+    return this.paymentsTypesRepository.findOne(payment_type_id);
   }
 }
