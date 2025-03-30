14888802d7e46f64507cf6a76ec257db04552661
JosueEC <josueev.cruz@gmail.com>
2025-03-29 22:36:22 -0600

    [FEAT](app.module): Inject the payments-types module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { PostgresConfigModule } from './database/postgres.config.module';
 
 import { PaymentsModule } from './payments/payments.module';
+import { PaymentsTypesModule } from './payments-types/payments-types.module';
 
 @Module({
-  imports: [PaymentsModule, PostgresConfigModule],
+  imports: [PaymentsModule, PostgresConfigModule, PaymentsTypesModule],
   controllers: [],
   providers: [],
 })
---
+++ src/rents-payments/dto/request/index.ts
@@ -0,0 +1,2 @@
+export * from './create-rent-payment.dto';
+export * from './update-rent-payment.dto';
