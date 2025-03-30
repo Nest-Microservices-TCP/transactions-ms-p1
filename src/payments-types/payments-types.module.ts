3bc625a30ac544d6929d771584138f0725fb1e15
JosueEC <josueev.cruz@gmail.com>
2025-03-29 22:35:18 -0600

    [FEAT](payments-types.module.): Inject the typeorm module configuration

--- src/payments-types/payments-types.module.ts
+++ src/payments-types/payments-types.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PaymentsTypesService } from './payments-types.service';
 import { PaymentsTypesController } from './payments-types.controller';
 import { PaymentsTypesRepository } from './repository/payments-types.repository';
 
+import { PaymentType } from './entity/payment-type.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([PaymentType])],
   providers: [PaymentsTypesRepository, PaymentsTypesService],
   controllers: [PaymentsTypesController],
 })
