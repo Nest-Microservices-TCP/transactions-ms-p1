08f72081767b3cbe8099b67ed4f12d8860ee4603
JosueEC <josueev.cruz@gmail.com>
2025-03-31 20:33:27 -0600

    [FEAT](payments-types.repository.interface): Implement from the repository-interface

--- src/payments-types/repository/interfaces/payments-types.repository.interface.ts
+++ src/payments-types/repository/interfaces/payments-types.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IBaseRepository } from 'src/common/repository';
+import { IRepository } from 'src/common/repository';
 import { CreatePaymentTypeRequest } from 'src/grpc/proto/transactions/payments_types.pb';
 import { PaymentType } from 'src/payments-types/entity/payment-type.entity';
 
 export interface IPaymentsTypesRepository
-  extends IBaseRepository<PaymentType, CreatePaymentTypeRequest> {}
+  extends IRepository<PaymentType, CreatePaymentTypeRequest> {}
